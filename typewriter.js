0bdde6dc644b3d6ee8a7cfe9254040363dadbbbe
Juliana Mota <julianamota89@gmail.com>
2019-02-11 21:57:09 +0100

    loop working

--- typewriter.js
+++ typewriter.js
@@ -1,31 +1,40 @@
 "use strict";
 
-const counter = 0;
+window.addEventListener("DOMContentLoaded", init);
+
 const text = document.querySelector("#typewriter").textContent;
-
+let counter = 0;
+const textL = text.length;
 console.log(text);
+let spaceSound = new Audio("typespace.mp3");
+console.log(spaceSound);
 
-let textcopy = " ";
+//let textcopy = " ";
 
 function init() {
   document.querySelector("#typewriter").textContent = " ";
+
+  console.log(textL);
+
+  loop();
 }
 
 function loop() {
-  const textL = text.length;
-  console.log(textL);
-  let counterText = text.split("");
-  console.log(counterText[5]);
+  let counterText = text.slice(0, counter + 1);
+  //console.log(counterText);
+  counter++;
 
-  for (let i = 0; i < counterText.length; i++) {
+  /*for (let i = 0; i < counterText.length; i++) {
     //console.log(counterText[i]);
     let letter = counterText[i];
     textcopy = textcopy + letter;
     console.log(textcopy);
     document.querySelector("#typewriter").textContent = textcopy;
-  }
-  document.querySelector("#typewriter").textContent = " ";
-}
+  }*/
 
-init();
-loop();
+  document.querySelector("#typewriter").textContent = counterText;
+
+  if (counter != textL) {
+    setTimeout(loop, 300);
+  }
+}
