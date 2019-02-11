357b785b1ff7e89ad0ff27c2d4de208bf93c9592
Juliana Mota <julianamota89@gmail.com>
2019-02-11 15:34:40 +0100

    console phrase working

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const counter = 0;
+const text = document.querySelector("#typewriter").textContent;
+
+console.log(text);
+
+let textcopy = " ";
+
+function init() {
+  document.querySelector("#typewriter").textContent = " ";
+}
+
+function loop() {
+  const textL = text.length;
+  console.log(textL);
+  let counterText = text.split("");
+  console.log(counterText[5]);
+
+  for (let i = 0; i < counterText.length; i++) {
+    //console.log(counterText[i]);
+    let letter = counterText[i];
+    textcopy = textcopy + letter;
+    console.log(textcopy);
+    document.querySelector("#typewriter").textContent = textcopy;
+  }
+  document.querySelector("#typewriter").textContent = " ";
+}
+
+init();
+loop();
